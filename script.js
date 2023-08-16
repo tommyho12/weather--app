94482bc4a42a0e0247eacf7f9117f10c319e8b80
Tommy Ho <tommyho@Tommys-MBP.attlocal.net>
2023-08-16 17:58:08 -0500

    added weather api

--- script.js
+++ script.js
@@ -14,3 +14,31 @@ var clrHistory = document.querySelector('#clrHistory');
 
 console.log(searchHistory.length);
 
+function renderWeather (city) {
+    var weatherSearch = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=a74f5b2196a4b1636be2b2efc00ce135&units=imperial'
+    
+    fetch(weatherSearch)
+    .then(function (res) {
+        if (!res.ok) throw new Error('There was an error!')
+        
+        return res.json();
+        })
+        .then(function (data) {
+            curCity.textContent = city;
+            //this for loop cycles through five days worth of forecasts
+            for (var i = 0; i < data.list.length; i += 7) {
+                var mainData = data.list[i];
+                var weatherData = data.list[i].weather[0];
+                
+                createWeatherCard(mainData, weatherData);
+            };
+            
+            curCity.textContent = city;
+            console.log(data.list[0]);
+        })
+        .catch(function (error) {
+            console.error(error);
+        });
+        
+};
+    
