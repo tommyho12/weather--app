b627da7e9df2fe4dab5a4f286622b474ae6fa47b
Tommy Ho <tommyho@Tommys-MBP.attlocal.net>
2023-08-16 17:52:56 -0500

    added a search history

---
+++ script.js
@@ -0,0 +1,16 @@
+var searchInput = document.querySelector('#search');
+var submitBtn = document.querySelector('#submitSearch');
+var historyBox = document.querySelector('#history');
+var curCity = document.querySelector('#curCity');
+var curIcon = document.querySelector('#icon');
+var curTempBox = document.querySelector('#curTemp');
+var curWind = document.querySelector('#curWind');
+var curHumid = document.querySelector('#curHumid');
+var forecastCont = document.querySelector('#forecastCont');
+var data;
+var searchHistory = [];
+var hisBox = document.querySelector('#hisBox');
+var clrHistory = document.querySelector('#clrHistory');
+
+console.log(searchHistory.length);
+
